2f1a65e78fcf6f5c1da4e552e67c3b1d05591ab0
Ben Godfrey <bcgodfrey91@gmail.com>
2017-01-10 19:57:00 -0700

    even MORE package json

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const jackals = require('./routes/jackals')
+const path = require('path');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/jackals', jackals)
 app.set('port', process.env.PORT || 3001);
+app.use(express.static(path.join(__dirname, 'client/build')))
 app.locals.title = 'Grudge List'
 
 app.listen(app.get('port'), () => {
