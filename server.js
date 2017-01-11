6b2576080eb9a2214b940740b373a913d4410f16
Ben Godfrey <bcgodfrey91@gmail.com>
2017-01-10 20:09:33 -0700

    adjust server

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/jackals', jackals)
 app.set('port', process.env.PORT || 3001);
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, '/public')))
 app.locals.title = 'Grudge List'
 
 app.listen(app.get('port'), () => {
