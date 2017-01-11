47e45d416c79ca5f188166b0de9f2ac3faa2aa15
Ben Godfrey <bcgodfrey91@gmail.com>
2017-01-11 09:45:06 -0700

    Remove old api calls

--- routes/jackals.js
+++ routes/jackals.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.locals.jackals = [{name: 'Pete', reason: 'Scumbag', fogiven: false, id: Date.now()}]
+app.locals.jackals = []
 
 router.get('/', (request, response) => {
   response.send({ jackals: app.locals.jackals })
--- src/components/JackalList.js
+++ src/components/JackalList.js
@@ -10,7 +10,7 @@ class JackalList extends Component {
       return(
         <div key={index}>
           <Link
-            to={`https://bg-grudge-bin-server.herokuapp.com/jackals/${jackal.id}`}
+            to={`/jackals/${jackal.id}`}
             className="link-to-jackal"
           >
             <h1>{jackal.name}</h1>
